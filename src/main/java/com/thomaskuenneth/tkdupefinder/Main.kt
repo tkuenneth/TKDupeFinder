2de50a2696c50a8576fec1d96196a88ac28518ad
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-11-15 13:50:12 +0100

    Launch the Swing app Kotlin-style

--- src/main/java/com/thomaskuenneth/tkdupefinder/Main.kt
+++ src/main/java/com/thomaskuenneth/tkdupefinder/Main.kt
@@ -1,5 +1,22 @@
 package com.thomaskuenneth.tkdupefinder
 
+import javax.swing.SwingUtilities
+import javax.swing.UIManager
+import javax.swing.UnsupportedLookAndFeelException
+
 fun main() {
-    println("Hello")
+    SwingUtilities.invokeLater {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+        } catch (thr: ClassNotFoundException) {
+            System.err.println(thr.localizedMessage)
+        } catch (thr: InstantiationException) {
+            System.err.println(thr.localizedMessage)
+        } catch (thr: IllegalAccessException) {
+            System.err.println(thr.localizedMessage)
+        } catch (thr: UnsupportedLookAndFeelException) {
+            System.err.println(thr.localizedMessage)
+        }
+        TKDupeFinderGUI().isVisible = true
+    }
 }
