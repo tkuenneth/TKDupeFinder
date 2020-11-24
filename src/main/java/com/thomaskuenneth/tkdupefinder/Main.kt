6a6785ea4b7a1b8f41604ee191a1c62c56eb4c3a
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2020-11-24 17:11:54 +0100

    First rough cut

--- src/main/java/com/thomaskuenneth/tkdupefinder/Main.kt
+++ src/main/java/com/thomaskuenneth/tkdupefinder/Main.kt
@@ -1,22 +1,94 @@
 package com.thomaskuenneth.tkdupefinder
 
-import javax.swing.SwingUtilities
-import javax.swing.UIManager
-import javax.swing.UnsupportedLookAndFeelException
+import androidx.compose.desktop.Window
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 
-fun main() {
-    SwingUtilities.invokeLater {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-        } catch (thr: ClassNotFoundException) {
-            System.err.println(thr.localizedMessage)
-        } catch (thr: InstantiationException) {
-            System.err.println(thr.localizedMessage)
-        } catch (thr: IllegalAccessException) {
-            System.err.println(thr.localizedMessage)
-        } catch (thr: UnsupportedLookAndFeelException) {
-            System.err.println(thr.localizedMessage)
+fun main() = Window(title = "TKDupeFinder", size = IntSize(600, 400)) {
+    MaterialTheme {
+        Column() {
+            FirstRow()
+            SecondRow()
+            ThirdRow()
         }
-        TKDupeFinderGUI().isVisible = true
+    }
+}
+
+@Composable
+fun FirstRow() {
+    val name = remember { mutableStateOf(TextFieldValue("")) }
+    Row(
+            modifier = Modifier.fillMaxWidth()
+                    .padding(8.dp),
+    ) {
+        TextField(
+                value = name.value,
+                placeholder = {
+                    Text("Base directory")
+                },
+                modifier = Modifier.alignBy(LastBaseline)
+                        .weight(1.0f),
+                onValueChange = {
+                    name.value = it
+                },
+        )
+        MySpacer()
+        Button(
+                onClick = {},
+                modifier = Modifier.alignByBaseline(),
+        ) {
+            Text("Find")
+        }
+    }
+}
+
+@Composable
+fun SecondRow() {
+    Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+                    .padding(8.dp),
+    ) {
+        Button(onClick = {}) {
+            Text("\u140A")
+        }
+        MySpacer()
+        Button(onClick = {}) {
+            Text("\u1405")
+        }
+        MySpacer()
+        Text(text = "1 of 42")
+    }
+}
+
+@Composable
+fun MySpacer() {
+    Spacer(modifier = Modifier.width(8.dp))
+}
+
+@Composable
+fun ThirdRow() {
+    val scrollState = rememberScrollState()
+    ScrollableColumn(
+            scrollState = scrollState,
+            modifier = Modifier.fillMaxSize().padding(8.dp),
+    ) {
+        Text("1")
+        Text("2")
+        Text("3")
     }
 }
