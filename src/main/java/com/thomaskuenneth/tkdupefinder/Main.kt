262cfc29a7886c0993798994c563c5a06773f771
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2020-11-27 12:58:29 +0100

    We now update the text field after a drop

--- src/main/java/com/thomaskuenneth/tkdupefinder/Main.kt
+++ src/main/java/com/thomaskuenneth/tkdupefinder/Main.kt
@@ -3,7 +3,6 @@ package com.thomaskuenneth.tkdupefinder
 import androidx.compose.desktop.AppManager
 import androidx.compose.desktop.AppWindow
 import androidx.compose.desktop.DesktopMaterialTheme
-import androidx.compose.desktop.Window
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -11,19 +10,15 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.layout.LastBaseline
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.KeyStroke
-import androidx.compose.ui.window.Menu
-import androidx.compose.ui.window.MenuBar
-import androidx.compose.ui.window.MenuItem
 import java.awt.datatransfer.DataFlavor
 import java.awt.dnd.DnDConstants
 import java.awt.dnd.DropTarget
@@ -31,49 +26,46 @@ import java.awt.dnd.DropTargetDropEvent
 import java.io.File
 import javax.swing.SwingUtilities.invokeLater
 
-
 fun main() {
-    AppManager.setMenu(
-            MenuBar(Menu("File", MenuItem(
-                    name = "Exit",
-                    onClick = { AppManager.exit() },
-                    shortcut = KeyStroke(Key.X)
-            )))
-    )
     invokeLater {
         AppWindow(title = "TKDupeFinder",
                 size = IntSize(600, 400)).show {
-            DesktopMaterialTheme {
-                Column() {
-                    FirstRow()
-                    SecondRow()
-                    ThirdRow()
-                }
-            }
+            TKDupeFinderContent()
         }
-        val target = object : DropTarget() {
-            @Synchronized
-            override fun drop(evt: DropTargetDropEvent) {
-                try {
-                    evt.acceptDrop(DnDConstants.ACTION_REFERENCE)
-                    val droppedFiles = evt
-                            .transferable.getTransferData(
-                                    DataFlavor.javaFileListFlavor) as List<*>
-                    for (file in droppedFiles) {
-                        println((file as File).absolutePath)
-                    }
-                } catch (ex: Exception) {
-                    ex.printStackTrace()
-                }
-            }
-        }
-        AppManager.windows.first().window.contentPane.dropTarget = target
     }
 }
 
 @Composable
-fun FirstRow() {
+fun TKDupeFinderContent() {
     val name = remember { mutableStateOf(TextFieldValue("")) }
+    DesktopMaterialTheme {
+        Column() {
+            FirstRow(name)
+            SecondRow()
+            ThirdRow()
+        }
+    }
+    val target = object : DropTarget() {
+        @Synchronized
+        override fun drop(evt: DropTargetDropEvent) {
+            try {
+                evt.acceptDrop(DnDConstants.ACTION_REFERENCE)
+                val droppedFiles = evt
+                        .transferable.getTransferData(
+                                DataFlavor.javaFileListFlavor) as List<*>
+                droppedFiles.first()?.let {
+                    name.value = TextFieldValue((it as File).absolutePath)
+                }
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            }
+        }
+    }
+    AppManager.windows.first().window.contentPane.dropTarget = target
+}
+
+@Composable
+fun FirstRow(name: MutableState<TextFieldValue>) {
     Row(
             modifier = Modifier.fillMaxWidth()
                     .padding(8.dp),
